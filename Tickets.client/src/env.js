d41a253906cf7acf8221e6f87ac8278bed60737c
Jacob Neitzell <107662321+JacobNeitzell@users.noreply.github.com>
2023-02-10 15:05:39 -0700

    Trying to get database

--- Tickets.client/src/env.js
+++ Tickets.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'https://localhost:7045' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'j-neitz.us.auth0.com'
+export const audience = 'https://classroom-projects.com'
+export const clientId = 'X5Df6BZjtVc1DoYN9ULAozFsX49HR0zE'
--- Tickets/Database/dbTickets.sql
+++ Tickets/Database/dbTickets.sql
@@ -1,1 +1,9 @@
 -- Active: 1675287303490@@SG-south-front-8653-7173-mysql-master.servers.mongodirector.com@3306@Sgroot
+
+CREATE TABLE
+    IF NOT EXISTS tickets(
+        ticketId INT NOT NULL primary key,
+        creatorId VARCHAR(255) NOT NULL,
+        ticketname VARCHAR(255),
+        ticketclient VARCHAR(255),
+    )
