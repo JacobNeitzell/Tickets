2f69300e69f0a6a532b005e338317d0c8a354611
Jacob Neitzell <107662321+JacobNeitzell@users.noreply.github.com>
2023-02-22 14:33:47 -0700

    Creating tickets

--- Tickets/Services/TicketsService.cs
+++ Tickets/Services/TicketsService.cs
@@ -2,5 +2,14 @@ namespace Tickets.Services;
 
 public class TicketsService
 {
+  private readonly TicketsRepository _tr;
+
+  public TicketsService(TicketsRepository tr)
+  {
+    _tr = tr;
+  }
+
+
+
 
 }
