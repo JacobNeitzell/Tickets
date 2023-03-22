81f48072c743725630c85fc34253ce25a9210157
Jacob Neitzell <107662321+JacobNeitzell@users.noreply.github.com>
2023-03-22 15:01:20 -0600

    Usertickets backend

--- Tickets/Repositories/UserTicketsRepository.cs
+++ Tickets/Repositories/UserTicketsRepository.cs
@@ -1,7 +1,38 @@
 namespace Tickets.Repositories;
 
-public class UserTicketsRepository
+public class UserTicketsRepository : BaseRepo
 {
+  public UserTicketsRepository(IDbConnection db) : base(db)
+  {
+
+  }
+
+  public UserTickets Create(UserTickets newTicket)
+  {
+    string sql = @"
+INSERT INTO usertickets
+(creatorId,ticketId)
+VALUES(@CreatorId,@TicketId)
+SELECT LAST_INSERT_ID()
+;";
+    int id = _db.ExecuteScalar<int>(sql, newTicket);
+    newTicket.Id = id;
+    return newTicket;
+
+  }
+
+
+  public void Delete(UserTickets savedTicket)
+  {
+    string sql = @"
+  DELETE FROM 
+  usertickets
+  WHERE id = @Id LIMIT 1
+  ;";
+    _db.Execute(sql, savedTicket);
+  }
+
+
 
 }
 
