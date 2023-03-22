9c9ce88aaed082ae73560c9a6daa122dbd2defca
Jacob Neitzell <107662321+JacobNeitzell@users.noreply.github.com>
2023-03-22 15:54:29 -0600

    Userticket service started

--- Tickets/Services/UserTicketsService.cs
+++ Tickets/Services/UserTicketsService.cs
@@ -2,8 +2,17 @@ namespace Tickets.Services;
 
 public class UserTicketsService
 {
+  private readonly TicketsRepository _tr;
 
+  private readonly UserTicketsRepository _ut;
 
+  private readonly TicketsService _ts;
 
+  public UserTicketsService(UserTicketsRepository ut, TicketsRepository tr, TicketsService ts)
+  {
+    _tr = tr;
+    _ut = ut;
+    _ts = ts;
+  }
 
 }
